e380097e73c6093ffe9c123728acfddfd792c1a1
Romain ODET <romain.odet@lecole-ldlc.com>
2018-11-18 15:20:27 +0100

    CSV functionality Menus Errors Decoder

--- README.txt
+++ README.txt
@@ -1,18 +1,22 @@
-Description : Calcul du nombre d'absence par étudiant à partir des log de connexion au réseau Wifi
-Notions : Lecture fichier, chaine de caractères, dictionnaire, liste
+# Calcul du nombre d'absence par étudiant à partir des log de connexion au réseau Wifi
+*Notions : Lecture fichier, chaine de caractères, dictionnaire, liste*
 Difficulté : BLEU
-Fonctionnalités possibles :
+## Fonctionnalités possibles :
  - Différentier les matins (8h-12h30) et les après-midi (13h30-18h) et afficher le nombre de journées et de demi-journée d'absence.
  - Ne pas tenir compte des jours ou personne n'est présent (il y a un jour férié dans le log !)
  - Générer un fichier csv contenant les statistiques
  - ...
 
-Votre nom :
+## Romain ODET
+*romain.odet@lecole-ldlc.com*
+**GITHUB REPO : https://github.com/romainodet/absences.git**
 
-Vos fonctionnalités :
- -
- -
- -
+## Vos fonctionnalités :
+- [x] Exporter les statistiques au format CSV
+- [x] Decodeur des fichiers csv générés
+- [ ] Ne pas tenir des jours ou personne n'est présent
+
+
 
 
 
--- absences.py
+++ absences.py
@@ -1,46 +1,93 @@
 import datetime
 import csv
 
-ofile = open('stats.csv', "w")
-writer = csv.writer(ofile, delimiter=',', quotechar=' ', quoting=csv.QUOTE_ALL)
-writer.writerow(['Login', 'abs'])
 
-LOG_FILE = "radius.log"  # file of the logs
-LOGIN_FILE = "login_1a_2015"  # all the login of the Students
+def read_abs(LOG_FILE, LOGIN_FILE):
+    presence = {}  # init the dictionary of the person which are present
+    statistics = {}  # init rhe dictionnary of the persons which are absents
 
-presence = {}  # init the dictionary of the person which are present
-statistics = {}  # init rhe dictionnary of the persons which are absents
+    try:
+        open(LOGIN_FILE, "r")
+        open(LOG_FILE, 'r')
+    except:
+        exit('One of the files doesn\'t exist')
+
+    for login in open(LOGIN_FILE, "r"):
+        statistics[login.strip()] = 0
+
+    for line in open(LOG_FILE):
+        if 'Auth: Login OK:' in line:
+            res = line.split(' : ')
+
+            dat = datetime.datetime.strptime(res[0], "%a %b %d %H:%M:%S %Y")
+            day = dat.strftime("%d-%m-%Y")
+            # Students are not supposed to be at school on week ends !
+            if dat.strftime('%a') == "Sat" or dat.strftime('%a') == "Sun":
+                continue
+
+            if day not in presence:
+                presence[day] = []
+
+            login = line[line.find("[") + 1:line.find("]")]
+            if login not in presence[day] and login in statistics:
+                presence[day].append(login)
+
+    for day in presence:
+        for student in statistics:
+            if student not in presence[day]:
+                print('Student %s was absent on %s' % (student, day))
+                statistics[student] += 1
+    with open('stats.csv', 'w', newline='') as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(['Login', 'Number of non-attendance'])
+        for login in sorted(statistics):
+            print("%s => %d" % (login, statistics[login]))
+            writer.writerow((login, statistics[login]))
 
 
-for login in open(LOGIN_FILE, "r"):
-    statistics[login.strip()] = 0
+def decoder_csv(file):
+    f = open(file, 'r')
 
-for line in open(LOG_FILE):
-    if 'Auth: Login OK:' in line:
-        res = line.split(' : ')
-
-        dat = datetime.datetime.strptime(res[0], "%a %b %d %H:%M:%S %Y")
-        day = dat.strftime("%d-%m-%Y")
-        # Students are not supposed to be at school on week ends !
-        if dat.strftime('%a') == "Sat" or dat.strftime('%a') == "Sun":
-            continue
-
-        if day not in presence:
-            presence[day] = []
-
-        login = line[line.find("[") + 1:line.find("]")]
-        if login not in presence[day] and login in statistics:
-            presence[day].append(login)
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        print('The student', row[0], 'was absent', row[1], 'time(s)')
+    f.close()
 
 
-for day in presence:
-    for student in statistics:
-        if student not in presence[day]:
-            print('Student %s was absent on %s' % (student, day))
-            statistics[student] += 1
+while True:
+    print('''
+     _      _  _____ _    ____  _____ ____  ____  ____  _____ ____ 
+    / \  /|/ \/    // \  /  _ \/  __//   _\/  _ \/  _ \/  __//  __\\
+    | |  ||| ||  __\| |  | | \||  \  |  /  | / \|| | \||  \  |  \/|
+    | |/\||| || |   | |  | |_/||  /_ |  \_ | \_/|| |_/||  /_ |    /
+    \_/  \|\_/\_/   \_/  \____/\____\\\\____/\____/\____/\____\\\\_/\_\\
 
-for login in sorted(statistics):
-    print("%s => %d" % (login, statistics[login]))
-    writer.writerow((login, statistics[login]))
+    Welcome in the wifi decoder/encoder :
 
-ofile.close()
+    2 modes are available :
+     TYPE 1 FOR THE DECODER
+     TYPE 2 FOR THE ENCODER
+
+    If you want to exit type 0
+
+     Please select one :                                                              
+    ''')
+    mode = None
+    try:
+        mode = int(input(' CHOOSE THE MODE : '))
+    except:
+        print('An error occured during the process. Please choose the number 1 for DECODER or 2 for ENCODER')
+
+    if mode == 1:
+        file = input('Please enter the name of the csv file to decode : ')
+        decoder_csv(file)
+    elif mode == 2:
+        # try
+        LOG_FILE = "radius.log"  # file of the logs
+        LOGIN_FILE = "login_1a_2015"  # all the login of the Students
+        read_abs(LOG_FILE, LOGIN_FILE)
+        # except:
+        # print('An error occured')
+    elif mode == 0:
+        exit('You ask to interrupt the program. GoodBye')
